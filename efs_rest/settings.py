bb90a5eb10c27effe4b00a63a8873a4984510151
john <jgarza@unomaha.edu>
2019-11-09 21:56:02 -0600

    Push 15

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -121,16 +121,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-("js", os.path.join(STATIC_ROOT, 'js')),
-("css", os.path.join(STATIC_ROOT, 'css')),
-("images", os.path.join(STATIC_ROOT, 'images')),
-("fonts", os.path.join(STATIC_ROOT, 'fonts')),
+    ("js", os.path.join(STATIC_ROOT, 'js')),
+    ("css", os.path.join(STATIC_ROOT, 'css')),
+    ("images", os.path.join(STATIC_ROOT, 'images')),
+    ("fonts", os.path.join(STATIC_ROOT, 'fonts')),
 )
 
 REST_FRAMEWORK = {
